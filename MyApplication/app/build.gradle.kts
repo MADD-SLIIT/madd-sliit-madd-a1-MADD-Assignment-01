4d89cf39e335f3d3e3978e9666ea84ab96525b9d
GhostVapor <kirujan3@gmail.com>
2024-09-26 18:01:10 +0530

    FirstCommit
    Finished up to first two page

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -1,20 +1,24 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.usedmart"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.usedmart"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -33,6 +37,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
@@ -42,7 +58,38 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    //implementation(libs.androidx.lifecycle.runtime.ktx)
+   // implementation(libs.androidx.activity.compose)
+    //implementation(platform(libs.androidx.compose.bom))
+    //implementation(libs.androidx.ui)
+   // implementation(libs.androidx.ui.graphics)
+   // implementation(libs.androidx.ui.tooling.preview)
+   // implementation(libs.androidx.material3)
+    implementation(libs.firebase.database)
+
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
+
+    //ViewModel
+    implementation("androidx.activity:activity-ktx:1.9.2")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
+    implementation("com.tbuonomo:dotsindicator:5.0")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ MyApplication/app/src/main/java/com/example/usedmart/activity/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.usedmart.activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.usedmart.Model.SliderModel
+import com.example.usedmart.SliderAdapter
+import com.example.usedmart.ViewModel.MainViewModel
+import com.example.usedmart.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel = MainViewModel()
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBanner()
+    }
+
+    private fun initBanner() {
+        binding.progressBarAd.visibility = View.VISIBLE
+        viewModel.banners.observe(this, Observer { items ->
+            banners(items)
+            binding.progressBarAd.visibility=View.GONE
+        })
+        viewModel.loadBanners()
+    }
+
+    private fun banners(images: List<SliderModel>) {
+        binding.viewPageSlider.adapter = SliderAdapter(images, binding.viewPageSlider)
+        binding.viewPageSlider.clipToPadding = false
+        binding.viewPageSlider.clipChildren = false
+        binding.viewPageSlider.offscreenPageLimit = 3
+        binding.viewPageSlider.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewPageSlider.setPageTransformer(compositePageTransformer)
+        if (images.size > 1) {
+            binding.dotIndicator.visibility = View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewPageSlider)
+        }
+    }
+}
