997ad7fca62c0ce49421fa27d20c333fd28ee681
GhostVapor <kirujan3@gmail.com>
2024-09-30 02:17:21 +0530

    Completed upto recommendation

--- MyApplication/app/src/main/java/com/example/usedmart/ViewModel/MainViewModel.kt
+++ MyApplication/app/src/main/java/com/example/usedmart/ViewModel/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.example.usedmart.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.usedmart.Model.BrandModel
+import com.example.usedmart.Model.ItemModel
 import com.example.usedmart.Model.SliderModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -10,22 +12,26 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 
-class MainViewModel():ViewModel() {
+class MainViewModel() : ViewModel() {
 
-    private val firebaseDatabase=FirebaseDatabase.getInstance()
+    private val firebaseDatabase = FirebaseDatabase.getInstance()
 
-    private val _banner=MutableLiveData<List<SliderModel>>()
+    private val _banner = MutableLiveData<List<SliderModel>>()
+    private val _brand = MutableLiveData<MutableList<BrandModel>>()
+    private val _recommend = MutableLiveData<MutableList<ItemModel>>()
 
+    val brands: LiveData<MutableList<BrandModel>> = _brand
     val banners: LiveData<List<SliderModel>> = _banner
+    val recommend: LiveData<MutableList<ItemModel>> = _recommend
 
-    fun loadBanners(){
-        val Ref=firebaseDatabase.getReference("Banner")
+    fun loadBanners() {
+        val Ref = firebaseDatabase.getReference("Banner")
         Ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val lists= mutableListOf<SliderModel>()
-                for (childSnapshot in snapshot.children){
-                    val list=childSnapshot.getValue(SliderModel::class.java)
-                    if (list!=null){
+                val lists = mutableListOf<SliderModel>()
+                for (childSnapshot in snapshot.children) {
+                    val list = childSnapshot.getValue(SliderModel::class.java)
+                    if (list != null) {
                         lists.add(list)
                     }
                 }
@@ -36,6 +42,50 @@ class MainViewModel():ViewModel() {
 
             }
 
+
+        })
+    }
+
+    fun loadBrand() {
+        val Ref = firebaseDatabase.getReference("Category")
+        Ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<BrandModel>()
+                for (childSnapshot in snapshot.children) {
+                    val list = childSnapshot.getValue(BrandModel::class.java)
+                    if (list != null) {
+                        lists.add(list)
+                    }
+                }
+
+                _brand.value = lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    fun loadRecomend() {
+        val Ref = firebaseDatabase.getReference("Items")
+        Ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<ItemModel>()
+                for (childSnapshot in snapshot.children) {
+                    val list = childSnapshot.getValue(ItemModel::class.java)
+                    if (list != null) {
+                        lists.add(list)
+                    }
+                }
+
+                _recommend.value = lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
 
         })
     }
